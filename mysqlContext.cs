db81e278b6a1cc1c82ad3dc32fc20139920a0780
Michalis <motisano@gmail.com>
2020-04-16 17:33:33 +0200

    Add files via upload

---
+++ mysqlContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Database_First_Try.Models;
+
+namespace Database_First_Try.DAL
+{
+    public class mysqlContext : IdentityDbContext<ApplicationUser>
+    {
+        public mysqlContext(DbContextOptions<mysqlContext> option): base(option) 
+        {
+            this.Database.EnsureCreated();
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public virtual DbSet<Person> Persons { get; set; }
+    }
+}
